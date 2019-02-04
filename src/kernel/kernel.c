5acff9181f3077d29ccb8faaabc71e1bd34127d0
Turner Vink <turner@turnervink.com>
2019-02-03 16:05:52 -0800

    Print welcome message to screen

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -20,8 +20,7 @@ void main(uint32_t r0, uint32_t r1, uint32_t atags)
 
 	hal_io_video_init();
 
-	hal_io_video_puts("HELLO RAFAEL", 0, 0xFFFFFFFF);
-	hal_io_video_puts("SHALL WE PLAY A GAME?", LINE_HEIGHT, 0xFFFFFFFF);
+	hal_io_video_puts("WELCOME TO JETOS", 0, 0xFFFFFFFF);
 
 	while (1) hal_io_serial_putc(hal_io_serial_getc());
 }
